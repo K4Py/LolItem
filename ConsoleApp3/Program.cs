06ca3f3207f1c86f82a7b80cb101c310afd0508e
3108848 <3108848@TE-GDA-D-D37H07>
2022-05-13 14:18:04 +0200

    Dodaj pliki projektów.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,25 @@
+﻿using LolClasses;
+
+//stwórz nowego champa - garena
+Garen g = new Garen();
+//opdal emotke dla garena
+g.ctrl3();
+
+//stworz instacje przedmiotu - Pickaxe
+Pickaxe pc = new Pickaxe();
+//sprawdz ile ma hp garen
+g.showAD();
+//kup mu item
+g.buyItem(pc);
+//sprawdz ile ma teraz hp
+g.showAD();
+//kup mu drugi item
+g.buyItem(pc);
+//sprawdz ile ma teraz hp
+g.showAD();
+
+
+
+
+
+
